1ba1647a955eaaff4dfc8cc0f9c752f7074d4740
Drishti <dishuchoudhary825@gmail.com>
2026-03-09 15:54:56 +0530

    update

---
+++ portfolio/task2.js
@@ -0,0 +1,21 @@
+function uniqueElements(arr1, arr2){
+
+let combined = arr1.concat(arr2);
+let unique = [];
+
+for(let i=0;i<combined.length;i++){
+
+if(unique.indexOf(combined[i]) === -1){
+unique.push(combined[i]);
+}
+
+}
+
+return unique;
+
+}
+
+let a = [1,2,3,4];
+let b = [3,4,5,6];
+
+console.log("Unique Elements:", uniqueElements(a,b));
